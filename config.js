d57f6de8f644607bfb158f711cc8caa81fbc25e7
Aayush Gupta <Aayush Gupta>
2021-06-23 20:34:45 +0530

    Initial Commit

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    team : "deadpool",
+    password : "6753a1138d31472881a035df8409ac58",
+    baseApi : "https://localhost:5001"
+}
+
+
+// module.exports = {
+//     team : "process.env.TEAM" || "BRUH",
+//     password : "process.env.PASSWORD" || "BRUHNNNNN",
+//     baseApi : "localhost:XXXX"
+// }
