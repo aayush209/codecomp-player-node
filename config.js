a9499bd506372e5eca52a01fd716b9b3594e07e3
Aayush Gupta <Aayush Gupta>
2021-06-27 23:19:39 +0530

    Added GCP files

--- config.js
+++ config.js
@@ -1,12 +1,5 @@
 module.exports = {
-    team : "deadpool",
-    password : "6753a1138d31472881a035df8409ac58",
+    team : "gladiator",
+    password : "99c35ced65cc461f8cc1e20ee306d5ff",
     baseApi : "https://mmd-2021-gameapi-g6titslaoq-ew.a.run.app"
 }
-
-
-// module.exports = {
-//     team : "process.env.TEAM" || "BRUH",
-//     password : "process.env.PASSWORD" || "BRUHNNNNN",
-//     baseApi : "localhost:XXXX"
-// }
